dc0c2ccf9e77c95198c68346683bb10c04134f79
jawad423 <jawad.jg2002@gmail.com>
2024-10-06 15:09:00 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+var registerform = document.querySelector(".registerform");
+var nameinput = document.querySelector("#name");
+var numberinput = document.querySelector("#number");
+var priceinput = document.querySelector("#price");
+
+
+var products = [];
+
+registerform.onsubmit = function(e){
+    e.preventDefault();
+
+    console.log("e");
+    var user = {
+        name: nameinput.value,
+        number: numberinput.value,
+        price: priceinput.value,
+    };
+
+
+    products.push(product);
+
+
+printdata();
+};
+
+function printdata(){
+    var data = `  `;
+    for(var i = 0; i < users.length; i++) {
+        data += 
+        `<tr>
+        <td>${products[i].name}</td>
+        <td>${products[i].number}</td>
+        <td>${products[i].price}</td>
+        </tr>`;
+    }
+
+   
+    document.querySelector("tbody").innerHTML = data;
+};
